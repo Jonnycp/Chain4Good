395569c75227241c91dbabf8a67208db6e6ecd8c
Jonnycp <jonathan-caputo@hotmail.com>
2025-12-08 09:20:09 +0100

    Loghi Chain4Good

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
   ScrollRestoration,
 } from "react-router";
 
-import type { Route } from "./+types/root";
+import type { Route } from "./types/root";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
@@ -19,16 +19,45 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap",
   },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="it">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Chain4Good</title>
+        <meta
+          name="description"
+          content="Chain4Good è il crowdfunding etico e trasparente. Traccia la tua donazione con la Blockchain, approva le spese e verifica l'impatto reale."
+        />
+        <meta
+          name="keywords"
+          content="Crowdfunding, donazioni, raccolta fondi, filantropia, beneficenza, enti no profit, cause sociali"
+        />
+        <meta name="robots" content="index, follow" />
+
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml"/>
+        <link rel="icon" href="/favicon.png" type="image/png"/>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="logos/logo-180x180.png"
+        />
+        <link rel="icon" sizes="192x192" href="logos/logo-192.png" />
+        <link rel="icon" sizes="512x512" href="logos/logo-512.png" />
+        <meta name="msapplication-TileImage" content="logos/logo-150.png" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="theme-color" content="#6AAB29" />
+
         <Meta />
         <Links />
       </head>
@@ -43,33 +72,4 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return <Outlet />;
-}
-
-export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = "Oops!";
-  let details = "An unexpected error occurred.";
-  let stack: string | undefined;
-
-  if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? "404" : "Error";
-    details =
-      error.status === 404
-        ? "The requested page could not be found."
-        : error.statusText || details;
-  } else if (import.meta.env.DEV && error && error instanceof Error) {
-    details = error.message;
-    stack = error.stack;
-  }
-
-  return (
-    <main className="pt-16 p-4 container mx-auto">
-      <h1>{message}</h1>
-      <p>{details}</p>
-      {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
-          <code>{stack}</code>
-        </pre>
-      )}
-    </main>
-  );
 }
