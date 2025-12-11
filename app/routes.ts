bcb5dab9365f5d99b4cabf9b2aad14b4b7da32b0
Luca <lugenba@gmail.com>
2025-12-11 14:29:52 +0100

    modifiche routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,13 +1,14 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  // Home Page
-  index("routes/home.tsx"),
+  // Home Page Utente
+  index("routes/HomePageUtente.tsx"),
   // Login
   route("login", "routes/login.tsx"),
   // Ente
-  route("profiloEnte", "routes/profiloEnte.tsx"),
+  route("profilo-ente", "routes/profiloEnte.tsx"),
   route("nuovo-progetto", "routes/NuovoProgetto.tsx"),
   route("ente-visibile", "routes/EnteVisibile.tsx"),
+  route("home-page-ente", "routes/HomePageEnte.tsx"),
 
 ] satisfies RouteConfig;
