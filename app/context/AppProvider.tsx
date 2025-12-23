dcbda4f4ab33a472e244af775ec4c995db4b34cf
Jonnycp <jonathan-caputo@hotmail.com>
2025-12-23 12:11:50 +0100

    Type project e fetch con react query

--- app/context/AppProvider.tsx
+++ app/context/AppProvider.tsx
@@ -1,29 +1,76 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import type { ReactNode } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
 
+export type Project = {
+  _id: string;
+  ente: {
+    id: string;
+    nome: string;
+    profilePicture: string;
+  },
+  titolo: string;
+  cover: string;
+  stato: "raccolta" | "attivo" | "completato" | "annullato";
+  luogo: string;
+  endDate: string;
+  targetAmount: number;
+  currentAmount: number;
+  numeroDonatori: number;
+  currency: string;
+  lastDonors: Array<{
+    id: string;
+    profilePicture: string;
+  }>;
+};
+
 interface AppContextType {
   user: {
     address: string;
     username: string;
     email?: string;
-    profilePicture?: string;
+    profilePicture: string;
     nonce: string;
     createdAt: Date;
     lastLogin?: Date;
+    isEnte: boolean;
+    enteDetails: {
+      nome: string;
+      denominazioneSociale: string;
+      descrizioneEnte: string;
+      indirizzoSedeLegale: string;
+      codiceFiscale: string;
+      partitaIva?: string;
+      sitoWeb?: string;
+    } | null;
   } | null;
+  projects: {
+    explore: Project[];
+    byCategory: Project[];
+    selectedCategory: string | null;
+  };
   loading: {
     user: boolean;
+    projectsExplore: boolean;
+    projectsByCategory: boolean;
   };
   refetchAll: () => void;
+  setCategory: (cat: string) => void;
 }
 
 const AppContext = createContext<AppContextType | undefined>(undefined);
 
-export default function AppProvider({ children, initialUser }: { children: ReactNode, initialUser?: AppContextType["user"] }) {
+export default function AppProvider({
+  children,
+  initialUser,
+}: {
+  children: ReactNode;
+  initialUser?: AppContextType["user"];
+}) {
+  //Query utente
   const {
     data: user,
     isLoading: userLoading,
@@ -44,13 +91,64 @@ export default function AppProvider({ children, initialUser }: { children: React
     retry: false,
   });
 
+  //Query progetti esplora
+  const {
+    data: projectsExplore,
+    isLoading: isLoadingExplore,
+    refetch: refetchProjectsExplore,
+  } = useQuery({
+    queryKey: ["projects", "explore"],
+    queryFn: async () => {
+     const res = await fetch(`${API_BASE_URL}/projects?limit=6&sort=endDate&order=asc`, {
+        credentials: "include",
+      });
+      if (res.status === 401) return window.location.reload();
+      if (!res.ok)
+        throw new Error("Errore durante il fetch dei progetti");
+      return res.json();
+    },
+    staleTime: 1000 * 60 * 5,
+    enabled: user !== null && !userLoading,
+  });
+
+  //Query progetti per categoria
+  const [selectedCategory, setSelectedCategory] = useState<string>("medical");
+
+  const {
+    data: projectsByCategory,
+    isLoading: isLoadingCategory,
+    refetch: refetchProjectsByCategory,
+  } = useQuery({
+    queryKey: ["projects", "category", selectedCategory],
+    queryFn: () =>
+      fetch(
+        `${API_BASE_URL}/projects?category=${selectedCategory}&limit=6&order=asc`, {
+        credentials: "include",
+      }
+      ).then((res) => res.json()),
+    staleTime: 1000 * 60 * 5,
+    enabled: user !== null && !userLoading,
+  });
+
   const value = {
     user: user || null,
     loading: {
       user: userLoading,
+      projectsExplore: isLoadingExplore,
+      projectsByCategory: isLoadingCategory,
+    },
+    projects: {
+      explore: projectsExplore || [],
+      byCategory: projectsByCategory || [],
+      selectedCategory,
+    },
+    setCategory: (cat: string) => {
+      setSelectedCategory(cat);
     },
     refetchAll: () => {
       refetchUser();
+      refetchProjectsExplore();
+      refetchProjectsByCategory();
     },
   };
 
