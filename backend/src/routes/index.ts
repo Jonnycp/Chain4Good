bcd9960b8fc1dc115c287e8973a1118f87835f82
Jonnycp <jonathan-caputo@hotmail.com>
2025-12-25 17:51:31 +0100

    Route ente

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes.ts';
 import projectRoutes from './project.routes.ts';
+import enteRoutes from './ente.routes.ts';
 
 const router = Router();
 
 router.use('/auth', authRoutes); 
 router.use('/projects', projectRoutes);
+router.use('/ente', enteRoutes);
 
 export default router;
