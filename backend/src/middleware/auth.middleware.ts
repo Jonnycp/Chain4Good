a5a0e82f22d4c2d554a0739738ec4a4b2bdbdc3c
Jonnycp <jonathan-caputo@hotmail.com>
2025-12-25 17:49:35 +0100

    Middleware isEnte

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,4 +1,5 @@
 import type { Request, Response, NextFunction } from "express";
+import { checkIfIsEnte } from "../services/blockchain.service.ts";
 
 export const isAuth = (req: Request, res: Response, next: NextFunction) => {
   if (!req.session || !req.session.address) {
@@ -9,3 +10,21 @@ export const isAuth = (req: Request, res: Response, next: NextFunction) => {
   }
   next();
 };
+
+export const isEnte = async (req: Request, res: Response, next: NextFunction) => {
+  if (!req.session || !req.session.address ) {
+    return res.status(401).json({ 
+      error: "Accesso negato. Devi essere autenticato con il tuo wallet.", 
+      code: 401
+    });
+  }
+  const isVerified = await checkIfIsEnte(req.session.address);
+  if (!isVerified) {
+    return res.status(403).json({ 
+      error: "Accesso negato: Non sei un ente autorizzato.",
+      code: 403 
+    });
+  }
+
+  next();
+}
