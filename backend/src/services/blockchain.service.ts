514c31c2697df31e0a3d9590019e3c4294da81f5
Jonnycp <jonathan-caputo@hotmail.com>
2026-01-04 01:30:36 +0100

    Generalizzato getContractAddress

--- backend/src/services/blockchain.service.ts
+++ backend/src/services/blockchain.service.ts
@@ -7,29 +7,33 @@ const minABI = ["function balanceOf(address owner) view returns (uint256)"];
 const RPC_URL = process.env.RPC_URL || "http://127.0.0.1:8545";
 
 /**
- * Legge l'indirizzo del contratto deployato da Ignition
+ * Legge l'indirizzo dei contratti deployati da Ignition
  */
-const getContractAddress = (): string => {
+export const getContractAddress = (): { enteNft: string; eurc: string; factory: string } | null => {
   try {
     const deploymentPath = path.resolve(
       process.cwd(),
       "../blockchain/ignition/deployments/chain-31337/deployed_addresses.json"
     );
-
+    
     if (fs.existsSync(deploymentPath)) {
       const data = JSON.parse(fs.readFileSync(deploymentPath, "utf-8"));
-      return data["EnteNFTModule#EnteNFT"];
+      return {
+        enteNft: data["EnteNFTModule#EnteNFT"],
+        eurc: data["Chain4Good#MockERC20"],
+        factory: data["Chain4Good#ProjectFactory"],
+      }
     }
   } catch (e) {
     console.error("Errore lettura indirizzo contratto:", e);
   }
-  return process.env.ENTE_NFT_ADDRESS || "";
+  return null;
 };
 
 export const checkIfIsEnte = async (walletAddress: string): Promise<boolean> => {
   try {
     const provider = new ethers.JsonRpcProvider(RPC_URL);
-    const contractAddress = getContractAddress();
+    const contractAddress = getContractAddress()?.enteNft;
     
     if (!contractAddress) {
       console.error("Indirizzo contratto NFT non trovato.");
